d7685657b62959b94fc5af2658efcde206543210
anahid <anahidmansoubi2015@gmail.com>
2017-08-05 01:06:17 +0430

    Add Some Validation in Model & Insert

--- apis/insert.js
+++ apis/insert.js
@@ -1,28 +1,82 @@
 /**
- * Created by anahid on 7/29/17.
+ * Created by mohammadmoradyar on 8/4/17.
  */
 var response = require("../common/const");
 
 var mongojs = require('mongojs');
 var db = mongojs('mongodb://localhost/SimpleServer');
 
-function provide(router) {
-    router.post('/insert/:model', function (req, res) {
-        try {
-            db.collection(req.params.model).insert(req.body, function (err) {
-                if (err) {
-                    console.log(err);
-                    res.send(response.DB_ERROR);
-                } else {
-                    var object_id = req.body._id;
-                    var returnResponse = response;
+var client = null;
+var body = null;
+var collectionName = null;
 
+function provide(router) {
+    router.post('/insert/:collectionName', function (req, res) {
+        try {
+            client = res;
+            body = req.body;
+            collectionName = req.params.collectionName;
+            var query = {main_record: true};
+            db.collection(collectionName).findOne(query, function (err, item) {
+                if (err) {
+                    client.send(response.DB_ERROR);
+                } else {
+                    if (!validateRequiredFields(item.username, body.username)) {
+                        res.send(response.BAD_BODY_ERROR);
+                    } else if (!validateRequiredFields(item.firstname, body.firstname)) {
+                        res.send(response.BAD_BODY_ERROR);
+                    } else if (!validateRequiredFields(item.lastname, body.lastname)) {
+                        res.send(response.BAD_BODY_ERROR);
+                    } else if (!validateRequiredFields(item.phone, body.phone)) {
+                        res.send(response.BAD_BODY_ERROR);
+                    } else {
+                        if (!validateTypeFormat(item.username, body.username)) {
+                            res.send(response.BAD_BODY_ERROR);
+                        } else if (!validateTypeFormat(item.firstname, body.firstname)) {
+                            res.send(response.BAD_BODY_ERROR);
+                        } else if (!validateTypeFormat(item.lastname, body.lastname)) {
+                            res.send(response.BAD_BODY_ERROR);
+                        } else if (!validateTypeFormat(item.phone, body.phone)) {
+                            res.send(response.BAD_BODY_ERROR);
+                        } else {
+                            db.collection(collectionName).insert(body, function (err) {
+                                if (err) {
+                                    console.log(err);
+                                    res.send(response.DB_ERROR);
+                                } else {
+                                    var object_id = req.body._id;
+                                    var returnResponse = response.SUCCESS_INSERT;
+                                    returnResponse.objectId = object_id;
+                                    res.send(returnResponse);
+                                }
+                            });
+                        }
+                    }
                 }
             });
         } catch (e) {
             console.log(response.GENERAL_ERROR);
         }
-
     });
 }
+function validateRequiredFields(item, value) {
+    if (item == "required") {
+        if (value == null) {
+            return false;
+        } else {
+            return true;
+        }
+    } else {
+        return true;
+    }
+}
+
+function validateTypeFormat(item, value) {
+    var itemType = typeof item;
+    var valueType = typeof value;
+    if (valueType === 'undefined')
+        return true;
+    return itemType == valueType;
+}
+
 exports.provide = provide;
--- apis/model.js
+++ apis/model.js
@@ -1,28 +1,53 @@
 /**
- * Created by anahid on 7/30/17.
+ * Created by mohammadmoradyar on 8/4/17.
  */
 var response = require("../common/const");
 
 var mongojs = require('mongojs');
 var db = mongojs('mongodb://localhost/SimpleServer');
 
+var client = null;
+var body = null;
+var collectionName = null;
+
 function provide(router) {
     router.post("/model", function (req, res) {
-        try {
+        client = res;
+        body = req.body;
+        collectionName = req.body.name;
+        db.getCollectionNames(collectionNamesCallBack)
+    })
+}
 
-            db.model.insert(req.body, function (err) {
-                if (err) {
-                    console.log(err);
-                    res.send(response.DB_ERROR);
+function collectionNamesCallBack(err, listOfCollections) {
+    if (err) {
+        client.send(response.DB_ERROR);
+    } else {
+        if (listOfCollections.indexOf(collectionName) > -1) {
+            // Model already exist
+            client.send(response.MODEL_ALREADY_EXIST);
+        } else {
+            // Model is new
+            var variable = "{";
+            for (var i = 0;i < body.fields.length; i++) {
+                if (body.fields[i].required) {
+                    variable += '"' + body.fields[i].name + '" : "required" ,';
                 } else {
-                    res.send(response.SUCCESS);
+                    variable += '"' + body.fields[i].name + '" : "not_required" ,';
                 }
-            });
-        } catch (e) {
-            console.log(response.GENERAL_ERROR);
+            }
+            variable += ' "main_record" : true}';
+            console.log(variable);
+            var doc = JSON.parse(variable);
+            db.collection(collectionName).insert(doc, function (err) {
+                if (err) {
+                    client.send(response.DB_ERROR);
+                } else {
+                    client.send(response.SUCCESS);
+                }
+            })
         }
-    });
+    }
 }
 
 exports.provide = provide;
-
